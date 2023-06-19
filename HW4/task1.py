9daa95e38407e911e405ad841faf54e46991933d
milesi <irina-rxv@yandex.ru>
2023-06-19 10:44:42 +0300

    Переделал транспонирование

--- HW4/task1.py
+++ HW4/task1.py
@@ -1,18 +1,8 @@
 # Транспонирование матрицы
 
 def transpose_matrix(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
-
-    # Создаем новую матрицу с перевернутыми размерностями
-    transposed_matrix = [[0 for _ in range(rows)] for _ in range(cols)]
-
-    # Заполняем новую матрицу значениями из исходной матрицы
-    for i in range(rows):
-        for j in range(cols):
-            transposed_matrix[j][i] = matrix[i][j]
-
-    return transposed_matrix
+    matrix_rotate = tuple(zip(*matrix[::-1]))
+    return matrix_rotate
 
 def print_matrix(matrix):
     for row in matrix:
